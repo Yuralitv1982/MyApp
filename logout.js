6692707931d6a6e5205923d26f125791bba41983
Yuralitv1982 <yuralitv02@gmail.com>
2025-04-27 17:01:10 +0300

    empty logout.js

--- logout.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authenticateToken = require('../middleware/auth'); // Шлях до вашого middleware
-
-//  b njve gjlj,yfz lkasjd;fljias
