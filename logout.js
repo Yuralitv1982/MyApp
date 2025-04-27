5b4b4c80f9d0073fac42aa3284bb0c342b324eca
Yuralitv1982 <yuralitv02@gmail.com>
2025-04-27 16:58:36 +0300

    logout in master branch

---
+++ logout.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const authenticateToken = require('../middleware/auth'); // Шлях до вашого middleware
+
+//  b njve gjlj,yfz lkasjd;fljias
